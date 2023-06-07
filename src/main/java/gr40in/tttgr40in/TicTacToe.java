44654b5a83c702ee6b6fc1bddfabbc5aecf06f7d
Rishat_Imangulov <gr40in@yandex.ru>
2023-06-08 00:16:14 +0500

    int desk to two boolean

--- src/main/java/gr40in/tttgr40in/TicTacToe.java
+++ src/main/java/gr40in/tttgr40in/TicTacToe.java
@@ -2,20 +2,27 @@ package gr40in.tttgr40in;
 
 import java.util.HashMap;
 
-public class TicTacToe implements Game{
+public class TicTacToe implements Game {
     static boolean gameIsRunned;
 
-    int [][] tttDesk;
+    boolean[][] tttDeskZero;
+    boolean[][] tttDeskCross;
+    int maxMoves = 3*3;
+    int currentMove;
+
     Player playerZero;
     Player playerCross;
 
-    public TicTacToe(int[][] tttDesk, Player playerZero, Player playerCross) {
-        this.tttDesk = new int[3][3];
+    public TicTacToe(Player playerZero, Player playerCross) {
+        this.tttDeskZero = new boolean[3][3];
+        this.tttDeskCross = new boolean[3][3];
+        this.currentMove = 0;
+
         this.playerZero = playerZero;
         this.playerCross = playerCross;
     }
 
-    private boolean CheckDisk(){
+    private boolean CheckDisk() {
         return false;
     }
 
